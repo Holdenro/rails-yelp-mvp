3e5c5fbdde135e06fd94fec18e4ba61dc60049d3
Ronan Recune <r.recune@gmail.com>
2019-08-15 00:30:02 +0200

    Challenge yelp rails day3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+category = ['chinese', 'italian', 'french', 'belgian', 'japanese']
+
+puts "creating 5 restaurant"
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: category.sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+end
+
+puts "success!" # check if seeds works when running in ruby.
